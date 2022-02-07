4adee09729d14923be01f8cc733453f6f0099392
PegasusMKD <csprogrammers@outlook.com>
2022-02-07 16:02:10 +0100

    Fixed minor issues and added path for UI

--- src/main/kotlin/finki/ukim/kgt/kgtregistry/KgtRegistryApplication.kt
+++ src/main/kotlin/finki/ukim/kgt/kgtregistry/KgtRegistryApplication.kt
@@ -2,8 +2,10 @@ package finki.ukim.kgt.kgtregistry
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class KgtRegistryApplication
 
 fun main(args: Array<String>) {
