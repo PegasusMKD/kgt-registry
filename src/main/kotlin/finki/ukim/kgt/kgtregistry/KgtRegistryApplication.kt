9cf4f67d18b1f68225dd327ea50d57c541c2a39a
PegasusMKD <csprogrammers@outlook.com>
2022-02-07 15:37:09 +0100

    Initial commit of registry

---
+++ src/main/kotlin/finki/ukim/kgt/kgtregistry/KgtRegistryApplication.kt
@@ -0,0 +1,11 @@
+package finki.ukim.kgt.kgtregistry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KgtRegistryApplication
+
+fun main(args: Array<String>) {
+    runApplication<KgtRegistryApplication>(*args)
+}
---
+++ src/test/kotlin/finki/ukim/kgt/kgtregistry/KgtRegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package finki.ukim.kgt.kgtregistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KgtRegistryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
